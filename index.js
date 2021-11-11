a6f7aae3ee8dc05a4fb876f7ae1a36005b3faf30
WilliamDenega <williamdenega@gmail.com>
2021-11-11 12:40:48 -0500

    Completed Assingment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+
+
+
+
+function writeCards(names, event){
+    const messages = [];
+    for(let i =0; i < names.length; i++){
+        messages[i] = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+
+    }
+
+    return messages;
+
+}
+
+
+function countDown(int){
+    while(int>=0){
+        console.log(int);
+        int--;
+    }
+}
